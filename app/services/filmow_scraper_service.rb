058a666b25e17f35f1dc4cda394b22eb764fc5fa
Pedro Guimaraes <peuvictor22@gmail.com>
2026-03-13 20:09:37 -0300

    concertando sintaxe no nokogiri

--- app/services/filmow_scraper_service.rb
+++ app/services/filmow_scraper_service.rb
@@ -11,21 +11,20 @@ class FilmowScraperService
   end
 
   def import
-    html = URI.open(@url, "User-Agent" => "Mozilla/5.0")
-    doc = Nokogiri::HTML(html)
+  html_raw = URI.open(@url, "User-Agent" => "Mozilla/5.0").read
+  doc = Nokogiri::HTML(html_raw, nil, 'UTF-8')
+  title_element = doc.at_css('h1.movie__title')
+  return nil unless title_element
 
-    # Pegando os dados (com os seletores que você descobriu!)
-    title_element = doc.at_css('h1.movie__title')
-    return nil unless title_element
-
-    {
-      title: title_element.text.split('(').first&.strip,
-      year: doc.at_css('.movie__year')&.text&.gsub(/[()]/, '')&.to_i,
-      rating: doc.at_css('.rating-badge__value')&.text&.strip&.to_f,
-      director: doc.at_css('a.movie__country-link')&.text&.strip,
-      filmow_url: @url
-    }
-  rescue
-    nil
-  end
+  {
+    title: title_element.text.split('(').first&.strip,
+    year: doc.at_css('.movie__year')&.text&.gsub(/[()]/, '')&.to_i,
+    rating: doc.at_css('.rating-badge__value')&.text&.strip&.to_f,
+    director: doc.at_css('a.movie__country-link')&.text&.strip,
+    filmow_url: @url
+  }
+rescue => e
+  puts "Erro no scraping: #{e.message}"
+  nil
+end
 end
