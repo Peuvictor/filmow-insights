8e71edf3b76f23dda492d3e090150e74fb3f2752
Pedro Guimaraes <peuvictor22@gmail.com>
2026-03-16 11:12:36 -0300

    Fix: Add robust headers to bypass 403 on production

--- app/services/filmow_scraper_service.rb
+++ app/services/filmow_scraper_service.rb
@@ -10,8 +10,15 @@ class FilmowScraperService
     @url = url
   end
 
-  def import
-  html_raw = URI.open(@url, "User-Agent" => "Mozilla/5.0").read
+ def import
+  options = {
+    "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Accept-Language" => "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Referer" => "https://filmow.com/"
+  }
+
+  html_raw = URI.open(@url, options).read
   doc = Nokogiri::HTML(html_raw, nil, 'UTF-8')
   title_element = doc.at_css('h1.movie__title')
   return nil unless title_element
